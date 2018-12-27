69f2a6d4e7da8bc6c40f83a11c9e30b98d9a0280
AndrzejKwiatkowski <andrzej_kwiatkowski@outlook.com>
2018-12-27 10:15:52 +0100

    LogOut additional function

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,6 +14,9 @@
         <li v-if="auth">
           <router-link to="/dashboard">Dashboard</router-link>
         </li>
+        <li v-if="auth">
+          <button @click="onLogout" class="logout">LogOut</button>
+        </li>
       </ul>
     </nav>
   </header>
@@ -24,7 +27,12 @@ export default {
     auth() {
       return this.$store.getters.isAuthenticated
     }
-  }
+  },
+  methods: {
+    onLogout(){
+      this.$store.dispatch('logout')
+    }
+  },
 }
 </script>
 <style scoped>
@@ -75,5 +83,12 @@ export default {
   li a:active,
   li a.router-link-active {
     color: #fa923f;
+  }
+  .logout {
+    background-color: transparent;
+    border: none;
+    font: inherit;
+    color: white;
+    cursor: pointer;
   }
 </style>
